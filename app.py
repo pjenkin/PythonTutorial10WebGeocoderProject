39f651e3224c7895c2648b46566b740d92309006
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-27 12:06:24 +0000

    22-241 WebGeocoder class moved to separate file and file upload checked

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from webgeocoder import WebGeocoder
 
 app = Flask(__name__)
 
 
-class WebGeocoder():
+# class WebGeocoder():
+#
+#     def __init__(self):
+#         """ constructor for WebGeocoder """
+#         if __name__ == '__main__':
+#             app.run(debug=True)
+#             ''' if this app were run by name, eg, to do SQLAlchemy model instantiation,
+#              then __main__ would not be the instance name '''
+#
+#     def upload(self, file):
+#         # flash(file.filename)
+#         print(file.filename)
+#         pass
 
-    def __init__(self):
-        """ constructor for WebGeocoder """
-        if __name__ == '__main__':
-            app.run(debug=True)
-            ''' if this app were run by name, eg, to do SQLAlchemy model instantiation,
-             then __main__ would not be the instance name '''
 
-    def upload(self):
-        pass
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/success_table', methods=['POSTs'])
+@app.route('/success_table', methods=['POST'])
 def success_table():
+    upload_file = request.files['file_name']
+    webgeocoder.upload(upload_file)
     return render_template('success-table.html')
 
 
+
+global webgeocoder
 webgeocoder = WebGeocoder()
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
+    ''' if this app were run by name, eg, to do SQLAlchemy model instantiation,
+     then __main__ would not be the instance name '''
 
---
+++ webgeocoder.py
@@ -0,0 +1,11 @@
+
+
+class WebGeocoder():
+
+    def __init__(self):
+        """ constructor for WebGeocoder """
+
+    def upload(self, file):
+        # flash(file.filename)
+        print(file.filename)
+        pass
