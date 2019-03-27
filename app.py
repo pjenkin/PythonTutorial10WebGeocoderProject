9cf16876d051f8bc96fb6aaf219788a07755e9c1
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-27 18:40:04 +0000

    22-241 Geocoding displayed on table-success page

--- app.py
+++ app.py
@@ -26,11 +26,33 @@ def index():
 
 @app.route('/success_table', methods=['POST'])
 def success_table():
-    upload_file = request.files['file_name']
-    webgeocoder.upload(upload_file)
-    return render_template('success-table.html')
+    try:
+        upload_file = request.files['file_name']
+    except Exception as exception:
+        return render_template('success-table.html', result_html=str(exception))
+    # webgeocoder.upload(upload_file)
+    try:
+        webgeocoder.upload_csv(upload_file)
+    except Exception as exception:
+        return render_template('success-table.html', result_html=str(exception))
+    # print(webgeocoder.html_from_dataframe())
+    try:
+        webgeocoder.geocode_dataframe()
+    except Exception as exception:
+        return render_template('success-table.html', result_html=str(exception))
+    try:
+        result_html = webgeocoder.html_from_dataframe()
+        print(result_html)
+        return render_template('success-table.html', result_html=result_html)
+    except Exception as exception:
+        return render_template('success-table.html', result_html=str(exception))
+    # print(webgeocoder.html_from_dataframe())
+    # print('should have printed result_html')
 
 
+
+
+    # TODO: find out correct exception types in each case and add a catch accordingly
 
 global webgeocoder
 webgeocoder = WebGeocoder()
