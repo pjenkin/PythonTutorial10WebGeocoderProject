aebf79a79a01b9ec7ba7193b766b4ccb5da29a79
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-27 17:36:42 +0000

    22-241 Geocoding with lat/lng tuple unpacked (using zip and *)

--- webgeocoder.py
+++ webgeocoder.py
@@ -1,11 +1,31 @@
-
+import pandas
+from geopy.geocoders import Nominatim
 
 class WebGeocoder():
+
+    uploaded_file = None        # property/class variable
+    dataframe = None
 
     def __init__(self):
         """ constructor for WebGeocoder """
 
     def upload(self, file):
-        # flash(file.filename)
-        print(file.filename)
-        pass
+        try:
+            print('Name of file uploaded: ' + file.filename)
+        except AttributeError:
+            print('This upload is not of a requests.file type')
+        self.uploaded_file = file
+
+    def upload_csv(self, file):
+        try:
+            print('Name of file uploaded: ' + file.filename)
+        except AttributeError:
+            print('This upload is not of a requests.file type')
+        self.uploaded_file = file
+        try:
+            dataframe = pandas.read_csv(file)
+            self.dataframe = dataframe
+            print(dataframe)
+        except Exception as exception:
+            print('Error in CSV import' + str(exception))
+
