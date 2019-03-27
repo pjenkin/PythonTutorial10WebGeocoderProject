31b4125c8dc030d0ad6c6281ec11d51bd02a47a2
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-27 11:10:29 +0000

    22-241 Flask serving index page; WebGeocoder class started

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+class WebGeocoder():
+
+    def __init__(self):
+        """ constructor for WebGeocoder """
+        if __name__ == '__main__':
+            app.run(debug=True)
+            ''' if this app were run by name, eg, to do SQLAlchemy model instantiation,
+             then __main__ would not be the instance name '''
+
+    def upload(self):
+        pass
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+webgeocoder = WebGeocoder()
+
+
+
+
