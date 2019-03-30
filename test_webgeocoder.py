4ba03a004388f5f0ea524110b6db827f756e2235
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-30 13:42:41 +0000

    22-241 Tidied up tests' code

--- test_webgeocoder.py
+++ test_webgeocoder.py
@@ -44,104 +44,42 @@ class WebGeocoderDataTestCase(unittest.TestCase):
         # arrange - (i) set dataframe, and (ii) filename of webgeocoder (non-private members)
         # act - (ii)  download a CSV, (iv) record CSV's filename (if poss)
         # assert - (v) that filenames (ii) and (iv) are equal
+        # NB uses HTTP to actually POST and then request by download a file from Flask server
 
 
         # arrange - (i) set dataframe, and (ii) filename of webgeocoder (non-private members)
         self.webgeocoder = WebGeocoder()
 
-        # https://pbpython.com/pandas-list-dict.html
-        test_data = [('ID', [1, 2, 3]),
-            ('Address', ['First x', 'Second x', 'Third x']),
-            ('Value_A', [10, 20, 30]),
-            ('Value_B', [12, 24, 36]),
-            ('Value_C', [8, 14, 96])
-            ]
-        dataframe = pandas.DataFrame.from_items(test_data)
-        self.webgeocoder.dataframe = dataframe
-        output = BytesIO()      # TextIOWrapper as per file input upload - https://stackoverflow.com/a/44764457
-        file = TextIOWrapper(output, encoding='cp1252', line_buffering=True)
         csv_string = '''ID,address,People
         1,44 Mead Crescent Southampton, 4
         2, 67 Trevingey Road, 3
         3, 66 Mount Pleasant Road Camborne,2'''
-        file.write(csv_string)
-        # print('---------------- readlines from frankenstein file')
-        # print(file.readlines())
-        file.flush()
-        file.seek(0, 0)         # https://docs.python.org/3/library/io.html
-        # print('---------------- readlines from frankenstein file')
-        # print(file.readlines())
         test_file_name = 'test_file.csv'
-        # file.name = file.filename = test_file_name          # set subclassed psuedo-file's name/filename attributes
-        file.name = test_file_name          # set subclassed psuedo-file's name/filename attributes
-        file.filename = test_file_name          # set subclassed psuedo-file's name/filename attributes
-
-        # print('---------------- readlines from frankenstein file')
-        # print(file.readlines())
-
-        file.seek(0, 0)
         # https://stackoverflow.com/a/33071581
-        # stream = StringIO(file.read())
-        # stream.seek(0, 0)
-        # stream = BytesIO(file)
-
-        # stream = open(file, 'rb', buffering=0)
-        # stream = open(csv_string, 'rb', buffering=0)
-        # stream.seek(0, 0)
-
-        # bytes = str.encode(csv_string)
         str_bytes = bytes(csv_string, 'utf-8')
-
-        # print('bytes: ' + bytes.decode(str_bytes))
-        # bytes = b'''ID, address, People
-        # 1,44 Mead Crescent Southampton, 4
-        # 2, 67 Trevingey Road, 3
-        # 3, 66 Mount Pleasant Road Camborne,2'''
-
-        # self.app.post('/', data=dict(file_name=file))
 
         data = {'name':'some name'}
         data['file_name']=(BytesIO(str_bytes), test_file_name)
         # https: // stackoverflow.com / a / 35712344
 
-        # self.app.post('/', data=dict(file_name=file), content_type='multipart/form-data')
         self.app.post('/', data=data, content_type='multipart/form-data')
-        # POST to index, using 'frankenstein file'   http://flask.pocoo.org/docs/0.12/testing/
-        print('&&&&&&& after POST &&&&&&&&&&&')
+        # POST to index, using bytes of CSV-like content
 
 
         # TODO:  tidy up without completely losing interesting bits
 
-        # self.webgeocoder.uploaded_file = file   # set mock-uploaded file to appropriate object
-        # self.webgeocoder.uploaded_file.filename = test_file_name
-        # print(self.webgeocoder.dataframe)
 
         # act - (ii)  download a CSV, (iv) record CSV's filename (if poss)
 
-        # print('###filename:  ' + self.webgeocoder.uploaded_file.filename)
-        # print('###filename:  ' + self.webgeocoder.get_uploaded_filename())
 
-        # downloaded_file = self.webgeocoder.download_csv_from_dataframe()
-        # downloaded_filename = downloaded_file.headers['Content-Disposition'].split('=')[-1]     # see sample data
         downloaded_file = self.app.get('/download')
-        print('downloaded file: ' + str(downloaded_file))
-        print('downloaded_file.data')
-        print(downloaded_file.data)
-        print(downloaded_file.headers)
-        print(downloaded_file.headers['Content-Disposition'])
-        print(re.findall('filename=(.+)', downloaded_file.headers['Content-Disposition'])[0])
         # regex to capture any characters after 'filename=' https://stackoverflow.com/a/31805472
 
         downloaded_filename = re.findall('filename=(.+)', downloaded_file.headers['Content-Disposition'])[0]
-        # print(downloaded_filename)
-        # print(test_file_name)
-        print(str.upper('This is the end of test_downloaded_filename which may or may not be working'))
-
-        # sample data: <Response 587 bytes [200 OK]>
-        # attachment; filename=geocoded_supermarkets_upper_case_Address.csv
 
         # assert names of download and upload (with 'geocoded_' prefix) are equal - could remove string
         self.assertEqual(downloaded_filename, 'geocoded_' + test_file_name)
+        print(str.upper('This is the end of test_downloaded_filename which may or may not be working'))
 
 
 
@@ -153,21 +91,13 @@ class WebGeocoderDataTestCase(unittest.TestCase):
         # arrange - (i) set up a file (actually io.TextIOWrapper) with name
         # act - (ii) upload (iii)  record CSV's filename (if poss)
         # assert - (iv) that filenames (i) and (iii) (of uploaded file) are equal
+        # NB does not use HTTP, file simulated upload only to WebGeocoder instance object
 
         # possibly should not use upload_csv , or maybe should...?
 
         # arrange - (i) set dataframe, and (ii) filename of webgeocoder (non-private members)
         self.webgeocoder = WebGeocoder()
 
-        # https://pbpython.com/pandas-list-dict.html
-        # test_data = [('ID', [1, 2, 3]),
-        #              ('name', ['First x', 'Second x', 'Third x']),
-        #              ('Value_A', [10, 20, 30]),
-        #              ('Value_B', [12, 24, 36]),
-        #              ('Value_C', [8, 14, 96])
-        #              ]
-        # dataframe = pandas.DataFrame.from_items(test_data)
-        # self.webgeocoder.dataframe = dataframe
         test_file_name = 'test_file.csv'
         output = BytesIO()  # TextIOWrapper as per file input upload - https://stackoverflow.com/a/44764457
         file = TextIOWrapper(output, encoding='cp1252', line_buffering=True)
@@ -189,64 +119,6 @@ class WebGeocoderDataTestCase(unittest.TestCase):
         self.assertEqual(uploaded_filename, test_file_name)
         print(str.upper('got here after assertion that uploaded filename is the same as test filename'))
 
-    #     # arrange - instantiate a file
-    #     # act - pass to GeoWebcoder's upload method
-    #     # assert file's
-    #
-    #     webgeocoder = WebGeocoder()
-    #
-    #     test_file_name = 'test-file.txt'
-    #
-    #     test_file = open(test_file_name, 'w')
-    #     test_file.write('Hello, here is a line of text text.')
-    #     test_file.close()
-    #     print(test_file)
-    #     with open(test_file_name, 'r') as test_file:
-    #         # x for exclusive file formation - opens or makes if non-existent https://stackoverflow.com/a/43081892
-    #         uploaded_file_name = webgeocoder.upload(test_file)
-    #
-    #     print(uploaded_file_name)
-    #     print(webgeocoder.get_uploaded_filename())
-    #
-    #     assert webgeocoder.get_uploaded_filename() == test_file_name
-    #     # end of test
-
-    # def test_uploaded_file_contents(self):
-    #     """test file upload name is correct"""
-    #     # arrange - instantiate a file
-    #     # act - pass to GeoWebcoder's upload method
-    #     # assert file's
-    #
-    #     webgeocoder = WebGeocoder()
-    #
-    #     test_file_name = 'test-file.txt'
-    #     test_string = 'Hello, here is a line of test text.'
-    #
-    #     test_file = open(test_file_name, 'w')
-    #     test_file.write(test_string)
-    #     test_file.close()
-    #     print(test_file)
-    #     with open(test_file_name, 'r') as test_file:
-    #         # x for exclusive file formation - opens or makes if non-existent https://stackoverflow.com/a/43081892
-    #         uploaded_file_name = webgeocoder.upload(test_file)
-    #
-    #     print(uploaded_file_name)
-    #     print(webgeocoder.get_uploaded_filename())
-    #
-    #     assert webgeocoder.get_uploaded_filename() == test_file_name
-    #     # end of test
-
-
-# # Flask tests should be in separate file
-# class SimpleTest(TestCase):
-#     def create_app(self):
-#         app = Flask(__name__)
-#         app.config['TESTING'] = True
-#         return app
-#
-#     def test_server_is_running_ok(self):
-#         response = urllib2.urlopen(self.get_server_url())
-#         self.assertEqual(response.code, 200)
 
 
 def webgeocoder_data_suite():
